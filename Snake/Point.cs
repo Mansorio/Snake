865416a032dd4db89f345d63803317032066855a
Mansorio <kuzminu@gmail.com>
2016-06-04 00:16:40 +0300

    Lesson 12

---
+++ Snake/Point.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+	class Point
+	{
+		public int x;
+		public int y;
+		public char sym;
+
+		public Point()
+		{
+		}
+
+		public Point(int _x, int _y, char _sym)
+		{
+			x = _x;
+			y = _y;
+			sym = _sym;
+		}
+
+        public Point(Point p) {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+
+        public void Move(int offset, Direction direction) {
+            switch (direction) {
+                case Direction.RIGHT:
+                    x += offset;
+                break;
+                case Direction.LEFT:
+                    x -= offset;
+                break;
+                case Direction.UP:
+                    y -= offset;
+                break;
+                case Direction.DOWN:
+                    y += offset;
+                break;
+
+            }
+           /* if (direction == Direction.RIGHT) {
+                x += offset;
+            } else if(direction == Direction.LEFT) {
+                x -= offset;
+            } else if (direction == Direction.UP) {
+                y -= offset;
+            } else if (direction == Direction.DOWN) {
+                y += offset;
+            }*/
+        }
+
+        public void Clear() {
+            sym = ' ';
+            Draw();
+        }
+
+        public bool IsHit(Point p) {
+            return p.x == this.x && p.y == this.y;
+        }
+
+		public void Draw()
+		{
+			Console.SetCursorPosition( x, y );
+			Console.Write( sym );			
+		}
+	}
+}
